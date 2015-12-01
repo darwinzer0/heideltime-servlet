09f1667d300f0422c27e164924470d713ff8c051
Ben Adams <darwinzero@gmail.com>
2015-12-02 10:45:11 +1300

    inital commit

---
+++ src/simiary/TaggerServletConfig.java
@@ -0,0 +1,9 @@
+package simiary;
+
+public class TaggerServletConfig {
+    public final static String treeTaggerHome = "~/treetagger";
+    public final static String chineseTokenizerPath = "~/treetagger/chinese_tokenizer";
+    public final static String stanfordModelPath = "/etc/stanford-pos/models/english-bidirectional-distsim.tagger";
+    public final static String stanfordConfigPath = "";
+
+}
